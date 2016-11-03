182f3f3dcb51fa19d49c81e32c7cd8e19343e251
OMGDuke <lex1989@gmail.com>
2016-11-03 22:17:26 +0000

    Implemented MainActivity code to display a random reponse string on button press. Improved on the tutorial code using a string array for easier localization

--- app/src/main/java/com/omgduke/magiceightball/MainActivity.java
+++ app/src/main/java/com/omgduke/magiceightball/MainActivity.java
@@ -1,15 +1,27 @@
 package com.omgduke.magiceightball;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    String[] fortuneList;
+
+
+    TextView mFortuneText;
+    Button mGenerateFortuneButton;
+    ImageView mFortuneBallImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,21 +30,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mFortuneText = (TextView) findViewById(R.id.fortuneText);
+        mFortuneBallImage = (ImageView) findViewById(R.id.fortunateImage);
+        mGenerateFortuneButton = (Button) findViewById(R.id.fortuneButton);
+        fortuneList = getResources().getStringArray(R.array.response_array);
+
+        mGenerateFortuneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                int index = new Random().nextInt(fortuneList.length);
+                mFortuneText.setText(fortuneList[index]);
+                YoYo.with(Techniques.Swing)
+                        .duration(500)
+                        .playOn(mFortuneBallImage);
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
     }
 
     @Override
